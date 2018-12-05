eec4941d2ddde5c6574e3ed16669da9ca352f34d
MatehElismar <31004078+MatehElismar@users.noreply.github.com>
2018-12-05 12:14:28 -0400

    Update HomeController.cs

--- Matehews/Controllers/HomeController.cs
+++ Matehews/Controllers/HomeController.cs
@@ -35,6 +35,11 @@ namespace Matehews.Controllers
               
             return View();
         }
+        
+        public IActionResult NotFound()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
